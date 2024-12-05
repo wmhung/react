a0d6abe66bb6467d5c3d214f2a44b15ea350b11d
Bruce <128801691+wmhung@users.noreply.github.com>
2024-12-05 10:35:42 +0800

    Add files via upload

---
+++ vite_d1_worldwise/src/pages/old/Homepage.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom';
+import AppNav from '../components/AppNav';
+import PageNav from '../components/PageNav';
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <AppNav />
+      <h1 className='test'>WorldWise</h1>
+      <Link to='/app'>Go to the app</Link>
+    </div>
+  );
+}
+
+export default Homepage;
---
+++ vite_d1_worldwise/src/pages/old/PageNotFound.jsx
@@ -0,0 +1,12 @@
+import PageNav from '../components/PageNav';
+
+function PageNotFound() {
+  return (
+    <div>
+      <PageNav />
+      <h1>PageNotFound</h1>
+    </div>
+  );
+}
+
+export default PageNotFound;
